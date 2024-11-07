6d2143404db720b812515cbb178379224282ce85
RayenManai <rayenmannai77@gmail.com>
2024-11-07 15:12:32 +0100

    init docker compose for postgres

--- migrations/V1__wahldaten.sql
+++ migrations/V1__wahldaten.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Partei (
     parteiId integer primary key,
     parteiName varchar(50) not null UNIQUE,
-    kurzbezeichnung varchar(10) not null UNIQUE,
-)
+    kurzbezeichnung varchar(10) not null UNIQUE
+);
 
 CREATE TABLE Wahlkreis (
     wahlkreisId integer primary key,
@@ -10,46 +10,38 @@ CREATE TABLE Wahlkreis (
     bundesland char(2) check (
         bundesland in ('BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV',
          'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH')),
-    anzahlwahlberechtigte integer,
-)
+    anzahlwahlberechtigte integer
+);
 
 CREATE TABLE DirektKandidat (
     DKId integer primary key,
     DKName varchar(50) not null,
     parteiId integer references Partei, -- NULL wenn der Kandidat zu keiner Partei gehört
-    wahlkreisId integer references Wahlkreis,
-)
+    wahlkreisId integer references Wahlkreis
+);
 
 CREATE TABLE StimmZettel (
     StimmzettelId integer primary key,
     jahr integer check (jahr in (2017, 2021, 2025)),
-    wahlkreisId integer references Wahlkreis,
-)
+    wahlkreisId integer references Wahlkreis
+);
 
 CREATE TABLE Erststimme (
     StimmzettelId integer primary key,
-    kandidatId integer references Direktkandidat check (
-        exists (
-            select * 
-            from Direktkandidat d, Stimmzettel s 
-            where kandidatId = d.DKId 
-                  and s.StimmzettelId = StimmzettelId 
-                  and d.wahlkreisId = s.wahlkreisId
-            )
-        ),
-)
+    kandidatId integer references Direktkandidat
+);
 
 CREATE TABLE Zweitstimme (
     StimmzettelId integer primary key,
-    parteiId integer references Partei not null,
-)
+    parteiId integer references Partei not null
+);
 
 CREATE TABLE ErststimmeErgebnisse (
     kandidatId integer references Direktkandidat,
     jahr integer,
     anzahlstimmen integer not null,
     primary key (kandidatId, jahr)
-)
+);
 
 CREATE TABLE ZweitstimmeErgebnisse (
     parteiId integer references Partei,
@@ -57,7 +49,7 @@ CREATE TABLE ZweitstimmeErgebnisse (
     jahr integer check (jahr in (2017, 2021, 2025)),
     anzahlstimmen integer not null,
     primary key (parteiId, wahlkreisId, jahr)
-)
+);
 
 CREATE TABLE Ergebnisse (
     parteiId integer references Partei,
@@ -67,4 +59,4 @@ CREATE TABLE Ergebnisse (
     ueberhangsMandate integer not null,
     ausgleichsMandate integer not null,
     primary key (parteiId, jahr)
-)
+);
