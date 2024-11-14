3ae520a448f277fb1fa5e34cfba32bee1d673b47
Mohamed <mohamed@deepscenario.com>
2024-11-14 15:41:27 +0100

    Added cvs and updated model to the new

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint
+from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint, Float, ForeignKeyConstraint
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -15,58 +15,72 @@ class Wahlkreis(Base):
     __tablename__ = 'Wahlkreis'
     wahlkreisId = Column(Integer, primary_key=True)
     wahlkreisName = Column(String(100), nullable=False, unique=True)
-    bundesland = Column(String(2), CheckConstraint(
-        "bundesland in ('BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH')"))
-    anzahlwahlberechtigte = Column(Integer)
+    bundesland = Column(String(2), ForeignKey('Bundesland.kurzbezeichnung'))
+
+class WalhkreisWahlInfo(Base):
+    __tablename__ = 'WalhkreisWahlInfo'
+    walhkreisId = Column(Integer, ForeignKey('Wahlkreis.wahlkreisId'), primary_key=True)
+    jahr = Column(Integer, primary_key=True)
+    anzahlWahlBerechtigte = Column(Integer, nullable=False)
+    anzahlWaehlende = Column(Integer, nullable=False)
+
+class Bundesland(Base):
+    __tablename__ = 'Bundesland'
+    kurzbezeichnung = Column(String(2), CheckConstraint(
+        "kurzbezeichnung in ('BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH')"),
+                             primary_key=True)
+    name = Column(String(100), nullable=False, unique=True)
+
+class BundeslandStruktur(Base):
+    __tablename__ = 'BundeslandStruktur'
+    kurzbezeichnung = Column(String(2), ForeignKey('Bundesland.kurzbezeichnung'), primary_key=True)
+    jahr= Column(Integer, primary_key=True)
+    bevoelkerung = Column(Integer)
+    flaeche = Column(Float)
 
 
-class DirektKandidat(Base):
-    __tablename__ = 'DirektKandidat'
-    DKId = Column(Integer, primary_key=True)
-    DKName = Column(String(100), nullable=False)
+class Kandidat(Base):
+    __tablename__ = 'Kandidat'
+    kandidatId = Column(Integer, primary_key=True)
+    titel = Column(String(15))
+    name = Column(String(100), nullable=False)
+    vorname = Column(String(100), nullable=False)
     parteiId = Column(Integer, ForeignKey('Partei.parteiId'))
+
+
+class DirektKandidatur(Base):
+    __tablename__ = 'DirektKandidatur'
+    kandidaturId = Column(Integer, primary_key=True)
+    kandidatId = Column(Integer, ForeignKey('Kandidat.kandidatId'))
     wahlkreisId = Column(Integer, ForeignKey('Wahlkreis.wahlkreisId'))
+    jahr = Column(Integer)
+    anzahlstimmen = Column(Integer, nullable=False)
 
 
-class StimmZettel(Base):
-    __tablename__ = 'StimmZettel'
-    StimmzettelId = Column(Integer, primary_key=True)
-    jahr = Column(Integer, CheckConstraint("jahr in (2017, 2021, 2025)"))
-    wahlkreisId = Column(Integer, ForeignKey('Wahlkreis.wahlkreisId'))
-
+class ParteiListe(Base):
+    __tablename__ = 'ParteiListe'
+    parteiId = Column(Integer, ForeignKey('Partei.parteiId'))
+    jahr = Column(Integer, primary_key=True)
+    kandidatId = Column(Integer, ForeignKey('Kandidat.kandidatId'), primary_key=True)
+    listenPlatz = Column(Integer)
 
 class Erststimme(Base):
     __tablename__ = 'Erststimme'
-    StimmzettelId = Column(Integer, primary_key=True)
-    kandidatId = Column(Integer, ForeignKey('DirektKandidat.DKId'))
-
+    id = Column(Integer, primary_key=True)
+    kanditaturId = Column(Integer, ForeignKey('DirektKandidatur.kanditaturId'))
 
 class Zweitstimme(Base):
     __tablename__ = 'Zweitstimme'
-    StimmzettelId = Column(Integer, primary_key=True)
-    parteiId = Column(Integer, ForeignKey('Partei.parteiId'), nullable=False)
-
-
-class ErststimmeErgebnisse(Base):
-    __tablename__ = 'ErststimmeErgebnisse'
-    kandidatId = Column(Integer, ForeignKey('DirektKandidat.DKId'), primary_key=True)
-    jahr = Column(Integer, primary_key=True)
-    anzahlstimmen = Column(Integer, nullable=False)
-
+    id = Column(Integer, primary_key=True)
+    ZSErgebnisId = Column(Integer, ForeignKey('ZweitstimmeErgebnisse.id'))
 
 class ZweitstimmeErgebnisse(Base):
     __tablename__ = 'ZweitstimmeErgebnisse'
-    parteiId = Column(Integer, ForeignKey('Partei.parteiId'), primary_key=True)
-    wahlkreisId = Column(Integer, ForeignKey('Wahlkreis.wahlkreisId'), primary_key=True)
-    jahr = Column(Integer, CheckConstraint("jahr in (2017, 2021, 2025)"), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    jahr = Column(Integer)
+    parteiId = Column(Integer, ForeignKey('Partei.parteiId'))
+    wahlkreisId = Column(Integer, ForeignKey('Wahlkreis.wahlkreisId'))
     anzahlstimmen = Column(Integer, nullable=False)
-
-    def __str__(self):
-        r = {'parteiId': self.parteiId,
-             'wahlkreisId': self.wahlkreisId,
-             'anzahlstimmen': self.anzahlstimmen,
-             'jahr': self.jahr}
-        return str(r)
 
 
 class Ergebnisse(Base):
