94e9eb8bd94524c78af1692aabd18285ca7bb761
Mohamed <mohamed@deepscenario.com>
2025-01-26 11:38:08 +0100

    Restored Q6 and added direktMandat to the results of one wahlkreis, corrected wahlbeteiligung in Bunedsland and wahlkreis

--- backend/rest/server.py
+++ backend/rest/server.py
@@ -1,12 +1,14 @@
 import logging
 import time
+from itertools import groupby
 
 from flask import Flask, jsonify, request, g
 from flask_cors import CORS
 
 from backend.utils.queries import (DATABASE_URL, create_engine, run_text_query, bundesweit, landesweit,
                                    beteiligung_bundesweit, wahlkreisweit, angeordnete_bundesweit,
-                                   angeordnete_landesweit, get_wahlkreis_data, get_wahlkreis_data_2)
+                                   angeordnete_landesweit, get_wahlkreis_data, get_wahlkreis_data_2,
+                                   beteiligung_landesweit, beteiligung_wahlkreis, knappste_sieger, direkt_mandat)
 
 log = True
 
@@ -59,7 +61,7 @@ def get_results(): #contains at most: year, bundesland, wahlkreis
         bundesland = values['bundesland']
         result1 = run_text_query(engine, landesweit, {'year': year, 'bundesland': bundesland}, output_format)
         result2 = run_text_query(engine, landesweit, {'year': 2017, 'bundesland': bundesland}, output_format) if year == 2021 else []
-        result3 = run_text_query(engine, beteiligung_bundesweit, {'year': year, 'bundesland': bundesland},
+        result3 = run_text_query(engine, beteiligung_landesweit, {'year': year, 'bundesland': bundesland},
                                  {0: ('wahlberechtigte', int), 1: ('wahlbeteiligte', int)})
         assert len(result3) == 1, "Something is wrong here!"
         return jsonify({'partiesResults': result1, 'partiesOldResults': result2,
@@ -71,11 +73,16 @@ def get_results(): #contains at most: year, bundesland, wahlkreis
     result1 = run_text_query(engine, wahlkreisweit, {'year': year, 'wahlkreis': wahlkreis}, output_format)
     result2 = run_text_query(engine, wahlkreisweit, {'year': year, 'wahlkreis': wahlkreis}, output_format) \
         if year == 2021 else []
-    result3 = run_text_query(engine, beteiligung_bundesweit, {'year': year, 'wahlkreis': wahlkreis},
+    result3 = run_text_query(engine, beteiligung_wahlkreis, {'year': year, 'wahlkreis': wahlkreis},
                              {0: ('wahlberechtigte', int), 1: ('wahlbeteiligte', int)})
     assert len(result3) == 1, "Something is wrong here!"
+    mandat = run_text_query(engine, direkt_mandat, {'year': year, 'wahlkreis': wahlkreis},
+                            {0: ('name', str), 1: ('party', str), 2: ('bundesland', str),
+                             3: ('direktMandat', bool), 4: ('UberhangMandat', bool)})
+    assert len(mandat) == 1, "Something is wrong here!"
     return jsonify({'partiesResults': result1, 'partiesOldResults': result2,
-                    'wahlberechtigte': result3[0]['wahlbeteiligte'],'wahlbeteiligte':result3[0]['wahlberechtigte']})
+                    'wahlberechtigte': result3[0]['wahlbeteiligte'],'wahlbeteiligte':result3[0]['wahlberechtigte'],
+                    'direktKandidat': mandat[0]})
 
 @app.route('/api/delegates', methods=['GET'])
 def get_delegates():
@@ -88,6 +95,21 @@ def get_delegates():
     else:
         result = run_text_query(engine, angeordnete_landesweit, {'year': values['year'], 'bundesland': values['bundesland']}, output_format)
     return jsonify({'abgeordnete': result}), 200
+
+
+@app.route('/api/q6', methods=['GET'])
+def get_knappste_sieger():
+    values = request.args.to_dict()
+    assert "year" in values.keys(), "year must be specified"
+    output_format = {0: ('name', str), 1: ('partei', str), 2: ('wahlkreis', str),
+                     3: ('gewonnene_stimmen', int), 4: ('sprung', int), 5:('vorg_name', str),
+                     6: ('vorg_partei', str), 7: ('vorg_anzahlstimmen', int), 8: ('ist_sieger', bool)}
+    result = run_text_query(engine, knappste_sieger, {'year': values['year']}, output_format)
+    sieger, verlierer = [], []
+    [sieger.append(r) if r['ist_sieger'] else verlierer.append(r) for r in result]
+    print({'knappsteSieger': sieger, 'knappsteVerlorene': verlierer})
+    return jsonify({'knappsteSieger': sieger, 'knappsteVerlorene': verlierer}), 200
+
 
 @app.route('/api/statistik2/<int:jahr>', methods=['GET'])
 def statistik_2(jahr):
